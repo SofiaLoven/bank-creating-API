f513cf1a6a8d12795a182042197156a1766e9bf0
SofiaLoven <sofia.loven93@gmail.com>
2023-05-19 17:19:47 +0200

    First push

---
+++ public/main.js
@@ -0,0 +1,216 @@
+
+///// Autentisering /////
+const loginForm = document.querySelector("#login");
+const user = document.querySelector("#user");
+const pass = document.querySelector("#pass");
+const welcome = document.querySelector("#welcome");
+const regForm = document.querySelector("#register");
+const regUser = document.querySelector("#regUser");
+const regPass = document.querySelector("#regPass");
+const regMess = document.querySelector("#regMess");
+const logout = document.querySelector("#logout");
+const bankPage = document.querySelector("#signedIn")
+
+let renderPage = async () =>{
+    let response = await fetch('/api/loggedin');
+    let data = await response.json();
+    //console.log(data)
+    if(data.user){
+        welcome.innerText = `You have signed in as ${data.user.user}!`;
+        loginForm.classList.add("hidden");
+        regForm.classList.add("hidden");
+        logout.classList.remove("hidden");
+        bankPage.classList.remove("hidden");
+        getAccounts(); 
+    } else{
+        console.log('Load Page:', data.error);
+    }
+}
+
+// Kolla att uanvändarnamnet inte än använt.
+regForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    let response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            user: regUser.value,
+            pass: regPass.value
+        })
+    })
+    let result = await response.json();
+    
+    if(response.ok){
+        regMess.innerText= `Tack för att du har registrerat dig. Nu kan ${regUser.value} logga in.`;
+        regUser.value ="";
+        regPass.value="";
+    }else{
+        alert('Ditt användarnamn är redan upptaget. Försök med ett nytt');
+        console.log('Error:', result);
+    }
+})
+
+loginForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    try{
+        let response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                loginUser: user.value,
+                loginPass: pass.value
+            })
+        })
+        let result = await response.json();
+        console.log('login:', result);
+        location.reload();
+    }catch{
+        res.status(401).json({ error: 'Unauthorized' });
+    }
+})
+
+logout.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    let response = await fetch('/api/logout',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}
+    });
+    let reply = await response.json();
+    console.log('logput:', reply)
+    location.reload();
+})
+
+///// Bankkonton /////
+
+//Hämtar alla konton
+let getAccounts = async ()=>{
+    try{
+        let response = await fetch('/api/bank');
+        let data = await response.json();
+
+        renderAccounts(data.accounts);
+    }catch(error){
+        console.log('Cant get accounts', error);
+    }
+}
+
+let accountList = document.querySelector('#accountList');
+//Skriver ut kontona i en lista
+let renderAccounts = (arr)=>{
+    accountList.innerHTML = '';
+    arr.forEach(obj => {
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <p>${obj.accountName}</p>
+        <p>Kontonummer ${obj._id}</p>
+        <p>Saldo ${obj.balance}kr</p>
+        <button id="open${obj._id}"> Redigera konto </button>
+        <form class='hidden' id="change${obj._id}" " data-postid="${obj._id}">
+        <label>Ange summa och välj om du vill ta ut eller sätta in pengar</label></br>
+        <select name="addRemove${obj._id}">
+        <option value="add">Sätt in</option>
+        <option value="remove">Ta ut</option>
+        </select></br>
+        <input type="number" id="sum${obj._id}" placeholder="kr">
+        <button>Skicka</button>
+        </form>
+        <button id="delete${obj._id}" class="hidden"> Radera konto </button>
+        `
+        accountList.append(li);
+
+        const openChangeBtn = document.querySelector(`#open${obj._id}`);
+        const changeForm = document.querySelector(`#change${obj._id}`);
+        const deleteBtn = document.querySelector(`#delete${obj._id}`);
+        
+        //Visar redigering av kontot.
+        openChangeBtn.addEventListener('click', ()=>{
+            changeForm.classList.remove("hidden");
+            deleteBtn.classList.remove("hidden");
+            openChangeBtn.classList.add("hidden");
+        });
+
+        //Ändrar saldot
+        changeForm.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            let select = document.querySelector(`select[name="addRemove${obj._id}"]`).value
+            let sum = parseInt(document.querySelector(`#sum${obj._id}`).value);
+            let firstBalance = parseInt(`${obj.balance}`);
+            let data = {sum: sum, select: select};
+
+            if(sum > firstBalance){
+                alert('Hallå där... Så mycket pengar har du inte på kontot!')
+            }else{
+                changeAccount(data, obj._id);
+            }
+        });
+
+        //Raderar kontot
+        deleteBtn.addEventListener('click', ()=>{deleteAccount(`${obj._id}`) })
+        
+    });
+};
+
+const changeAccount = async(data, id)=>{
+    try{
+        let response = await fetch(`/api/bank/${id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        //console.log('Success:', result);
+        location.reload();
+    }catch(error){
+        console.log('error', error);
+    }
+};
+
+const deleteAccount = async(id) =>{
+    try{
+        let response = await fetch(`/api/bank/${id}`,{ method: 'DELETE'});
+        const result = await response.json();
+        console.log('Sucess', result);
+        location.reload();
+    }catch(error){
+        console.log('error', error);
+    }
+}
+
+// Lägger till nya konton via form.
+let createForm = document.querySelector("#addAccount");
+let accountName = document.querySelector('#name');
+let accountBalance = document.querySelector('#balance');
+
+createForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let data = {
+        name: accountName.value,
+        balance: accountBalance.value,
+    }
+    //console.log(data);
+    addAccount(data);
+    //Render site
+    accountName.value ='';
+    accountBalance.value ='';
+})
+
+let addAccount = async(data)=>{
+    try{
+        let response = await fetch('/api/bank',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        console.log('Success:', result);
+        location.reload();
+    }catch(error){
+        console.log('error', error);
+    }
+};
+
+renderPage();
---
+++ server.js
@@ -0,0 +1,137 @@
+import { MongoClient, ObjectId } from 'mongodb'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import bcrypt from 'bcrypt';
+
+const app = express();
+const port = 3001;
+const FIVE_MINUTES = 5 * 60 * 1000; 
+const saltRounds = 10;
+
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+await client.connect();
+const db = client.db('bank');
+const accountCollection = db.collection('accounts');
+const dbLogin = client.db('loginproject');
+const userCollection = dbLogin.collection('users');
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.urlencoded());
+app.use(session({
+    resave:false,
+    saveUninitalialized: false,
+    secret: 'Shhh',
+    cookies:{
+        maxAge:  FIVE_MINUTES
+    }
+}));
+
+//////////////////  Autentisering  ////////////////////
+
+app.post('/api/register', async (req, res)=>{
+    const allUserNames = await userCollection.find({}).toArray();
+    const result = allUserNames.filter(user=> req.body.user === user.user);
+
+    if(result.length === 0){
+        const hashPass = await bcrypt.hash(req.body.pass, saltRounds);
+        const post = {
+            user: req.body.user, 
+            pass: hashPass,
+        };
+        let answer = await userCollection.insertOne(post);
+        res.json({answer});
+    }else{
+        res.status(401).json({error: 'Username used'})
+    }    
+})
+
+
+app.post('/api/login', async (req, res)=>{
+    const user = await userCollection.findOne({user: req.body.loginUser});
+    
+    const match = await bcrypt.compare(req.body.loginPass, user.pass); 
+    
+    console.log("match:", user.pass);
+    if(match){
+        req.session.user = user;
+        res.json({user: user.user});
+        console.log(user.user);
+    }else{
+        res.status(401).json({ error: 'Unauthorized'});
+    }
+})
+
+app.get('/api/loggedin', (req, res)=>{ 
+    if(req.session.user){
+        res.json({user: req.session.user});
+    }else{
+        res.status(401).json({ error: 'Unauthorized'});
+    }
+});
+
+app.post('/api/logout', (req,res)=>{
+    req.session.destroy(()=>{
+        res.json({
+            loggedin:false
+        });
+    });
+});
+
+//////////////////  Banken  ////////////////////
+
+//Hämtar alla konton
+app.get('/api/bank', async(req, res)=>{
+    const answer = await accountCollection.find({}).toArray();
+    res.json({accounts: answer});
+});
+
+//Hämta ett konto
+app.get('/api/bank/:id', async(req, res)=>{
+    const answer = await accountCollection.findOne({_id: new ObjectId(req.params.id)});
+    res.json({accounts: answer});
+});
+
+//Skapa nytt konto. 
+app.post('/api/bank', async ( req, res)=>{
+    const post = {
+        accountName: req.body.name, 
+        balance: req.body.balance,
+        registered: new Date(),
+    };
+    let answer = await accountCollection.insertOne(post);
+    res.json({answer});
+});
+
+//uppdatera ett konto
+app.put('/api/bank/:id', async(req, res)=>{
+    const temp = new ObjectId(req.params.id);
+    const balanceBefore = await accountCollection.findOne({_id: temp});
+    let balanceAfter 
+    if(req.body.select === 'add'){
+        balanceAfter = parseInt(balanceBefore.balance) + parseInt(req.body.sum);
+
+    } else{
+        balanceAfter = parseInt(balanceBefore.balance) - parseInt(req.body.sum);
+    };
+
+    const post = {balance: balanceAfter};
+    await accountCollection.updateOne({_id: temp}, { $set: post});
+    const updatedBalance = await accountCollection.findOne({_id:temp });
+    res.json({updatedBalance});
+})
+
+//Tar bort ett konto
+app.delete('/api/bank/:id', async(req, res)=>{
+    const deleted = await accountCollection.deleteOne({_id: new ObjectId(req.params.id)});
+    res.json({deleted});
+})
+
+
+
+//lyssnar på hemsidan.
+app.listen(port, ()=>{
+    console.log( 'app listening to port', port)
+});
